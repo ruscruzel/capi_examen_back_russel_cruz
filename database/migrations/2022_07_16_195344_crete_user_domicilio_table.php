4028a742d3b2ae7a7ebf148ff77d6ff2e0f65f74
ruscruzel <ruscruzel@gmail.com>
2022-07-16 15:16:18 -0500

    migracion user_domicilio

---
+++ database/migrations/2022_07_16_195344_crete_user_domicilio_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_domicilio', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id');
+            $table->string('domicilio');
+            $table->integer('nuero_exterior');
+            $table->string('colonia');
+            $table->integer('cp');
+            $table->string('ciudad');
+            $table->boolean('activo')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_domicilio');
+    }
+};
